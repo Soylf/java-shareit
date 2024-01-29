2f8e116214962d29ade77e6a6953e16d2169e57b
Soyl <Soylgr@yandex.ru>
2024-01-29 20:50:31 +0300

    fix CheckStyle

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.service.UserService;
-
 import java.util.List;
 import java.util.Optional;
 
