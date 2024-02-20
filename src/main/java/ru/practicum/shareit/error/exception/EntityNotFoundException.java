a222459e17d71d1e65a34b08fe4c38d3894b4651
Soyl <Soylgr@yandex.ru>
2024-02-20 16:00:12 +0300

    CheckStyle

--- src/main/java/ru/practicum/shareit/error/exception/EntityNotFoundException.java
+++ src/main/java/ru/practicum/shareit/error/exception/EntityNotFoundException.java
@@ -1,5 +1,4 @@
 package ru.practicum.shareit.error.exception;
-
 public class EntityNotFoundException extends IllegalStateException {
     public EntityNotFoundException(String message) {
         super(message);
