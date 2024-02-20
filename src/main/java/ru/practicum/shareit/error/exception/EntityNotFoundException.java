c80d09ed6ffd99126465e32b96cb85b37b39991b
Soyl <Soylgr@yandex.ru>
2024-02-20 16:01:55 +0300

    CheckStyle

--- src/main/java/ru/practicum/shareit/error/exception/EntityNotFoundException.java
+++ src/main/java/ru/practicum/shareit/error/exception/EntityNotFoundException.java
@@ -1,4 +1,5 @@
 package ru.practicum.shareit.error.exception;
+
 public class EntityNotFoundException extends IllegalStateException {
     public EntityNotFoundException(String message) {
         super(message);
--- src/main/java/ru/practicum/shareit/error/exception/NoEnumValueArgumentException.java
+++ src/main/java/ru/practicum/shareit/error/exception/NoEnumValueArgumentException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.error.exception;
 
-public class NoEnumValueArgumentException extends RuntimeException{
+public class NoEnumValueArgumentException extends RuntimeException {
     public NoEnumValueArgumentException(String message) {
         super(message);
     }
