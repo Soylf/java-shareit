b55138fb565d8514b9a43e55f1b8b8a7fa56a097
Soyl <Soylgr@yandex.ru>
2024-03-15 08:12:42 +0300

    fix

--- gateway/src/main/java/ru/practicum/shareit/user/UserController.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,10 @@
 package ru.practicum.shareit.user;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.shareit.error.exception.BadRequestException;
 import ru.practicum.shareit.user.dto.UserDto;
 
 import javax.validation.Valid;
@@ -14,7 +12,6 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping(path = "/users")
 @RequiredArgsConstructor
-@Slf4j
 @Validated
 public class UserController {
     private final UserClient client;
@@ -27,16 +24,15 @@ public class UserController {
     @PatchMapping("/{userId}")
     public ResponseEntity<Object> update(@Valid @RequestBody UserDto userDto,
                                          @PathVariable("userId") Long id) {
-        checkValidUserForUpdate(userDto);
         return client.update(id, userDto);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Object> getUser(@PathVariable("id") Long id) {
+    @GetMapping("/{userId}")
+    public ResponseEntity<Object> getUser(@PathVariable("userId") Long id) {
         return client.getUser(id);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping()
     public ResponseEntity<Object> getUsers() {
         return client.getUsers();
     }
@@ -44,19 +40,5 @@ public class UserController {
     @DeleteMapping
     public ResponseEntity<Object> delete(@PathVariable("id") Long id) {
         return client.delete(id);
-    }
-
-    private void checkValidUserForUpdate(UserDto user) {
-        if (user.getEmail() != null) {
-            if (user.getEmail().isBlank() || !user.getEmail().matches(".+[@].+[\\.].+")) {
-                throw new BadRequestException("Email");
-            }
-        }
-
-        if (user.getName() != null) {
-            if (user.getName().isBlank()) {
-                throw new BadRequestException("Name");
-            }
-        }
     }
 }
