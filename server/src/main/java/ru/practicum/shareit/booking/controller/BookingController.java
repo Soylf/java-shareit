0176e0bf9e491954d8ea263ce1038c87f818941c
Soyl <Soylgr@yandex.ru>
2024-03-17 11:51:30 +0300

    fix

--- server/src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ server/src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -29,7 +29,7 @@ public class BookingController {
 
     @PatchMapping("/{bookingId}")
     public BookingDto setApprove(@RequestHeader("X-Sharer-User-Id") Long userId, @PathVariable Long bookingId,
-                                 @RequestParam(name = "approved", required = false) Boolean approved) {
+                                 @RequestParam(name = "approved") Boolean approved) {
         log.info("Полчен запрос на обновление бронирования от " + userId + " на бронь  {}", bookingId);
         return service.updateStatus(userId, bookingId, approved);
     }
